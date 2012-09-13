4bc4d1805ec68a23b6e93889c412acdf9254322c
holden <holden@b80b7f44-204b-4dd4-b6e5-4114e6d279db>
2012-09-13 08:50:34 +0000

    Started dev for HT palm plugin
    git-svn-id: https://svn.epfl.ch/svn/htpalm-plugin/trunk@2 b80b7f44-204b-4dd4-b6e5-4114e6d279db

---
+++ src/HTPALM_Plugin/SpiralMosiaic.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ * Karl Bellve
+ * Biomedical Imaging Group
+ * University of Massachusetts Medical School
+ * Karl.Bellve@umassmed.edu
+ * http://big.umassmed.edu/
+ *
+ */
+
+package HTPALM_Plugin;
+import java.util.Vector;
+
+
+public class SpiralMosiaic{
+   
+   private static final int NDIM = 2;
+	
+   private double pos_start[] = new double[NDIM];
+   private double mosaicBox[] = new double[4]; 
+   private int nFOV_total, nFOV_current;
+   FOV[] FOVArray;
+
+   class FOV{
+      private double[] pos = new double[NDIM];
+      private Boolean skipFOV;
+      FOV(){}
+     
+      void initialize(double[] pos, Boolean skipFOV){
+         this.pos = pos;
+         this.skipFOV = skipFOV;
+      }
+      
+      void setPos(double[] pos){
+         this.pos = pos;
+      }
+      
+      double[] getPos(){
+         return pos;
+      }
+
+      public Boolean getSkipFOV() {
+         return skipFOV;
+      }
+      
+      public void setSkipFOV(Boolean skipFOV) {
+         this.skipFOV = skipFOV;
+      }
+   }
+           
+   public SpiralMosiaic(){}
+   
+   public void initialize(double posStart_X, double posStart_Y, double[] mosaicBox, int nFOV_total){
+      double[] pos = new double[NDIM];
+      pos[0] = posStart_X;
+      pos[1] = posStart_Y;
+      this.initialize(pos, mosaicBox,nFOV_total);
+   }
+
+   public void initialize(double[] posStart, double[] mosaicBox, int nFOV_total){
+      this.nFOV_total = nFOV_total;
+      this.pos_start = posStart;
+      this.mosaicBox = mosaicBox;
+      this.FOVArray = new FOV[nFOV_total];
+
+      this.setupFOV();
+   }
+
+   private void setupFOV(){
+      //TODO Assign the positions here
+   }
+   
+   public double[] getFOVPos(int nFOV){
+      return this.FOVArray[nFOV].getPos();
+   }
+
+   public void setSkipWell(int nFOV, Boolean skipVal){
+     this.FOVArray[nFOV].setSkipFOV(skipVal); 
+   }
+
+   public Boolean getSkipWell(int nFOV){
+      return this.FOVArray[nFOV].getSkipFOV();
+   }
+           
+}
+
+
+
+
